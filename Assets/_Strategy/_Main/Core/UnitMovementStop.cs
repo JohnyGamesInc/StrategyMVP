aea6e648f236c284f40de571159db2500452d7b6
Evgenii <ivanjohnoff@gmail.com>
2023-08-09 11:28:52 +0300

    add add unit stop on collision

--- Assets/_Strategy/_Main/Core/UnitMovementStop.cs
+++ Assets/_Strategy/_Main/Core/UnitMovementStop.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using _Strategy._Main.Abstractions;
 using _Strategy._Main.Utils.AsyncExtensions;
+using _Strategy._Main.Utils.GOExtensions;
+using UniRx;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -12,13 +15,47 @@ namespace _Strategy._Main.Core
     {
         
         [SerializeField] private NavMeshAgent _agent;
+        [SerializeField] private CollisionDetector _collisionDetector;
+
+        [SerializeField] private int _throttleFrames = 60;
+        [SerializeField] private int _continiusThreshold = 10;
         
         public event Action OnStop;
+        
+        public IAwaiter<AsyncExtensions.Void> GetAwaiter() => new StopAwaiter(this);
 
-
+        
+        
         private void OnValidate() => _agent ??= GetComponent<NavMeshAgent>();
-        
-        
+
+
+        private void Awake()
+        {
+            _collisionDetector.Collisions
+                .Where(_ => _agent.hasPath)
+                .Where(collision => collision.collider.GetComponentInParent<IUnit>() != null)
+                .Select(_ => Time.frameCount)
+                .Distinct()
+                .Buffer(_throttleFrames)
+                .Where(buffer =>
+                {
+                    for (int i = 1; i < buffer.Count; i++)
+                    {
+                        if (buffer[i] - buffer[i - 1] > _continiusThreshold)
+                            return false;
+                    }
+                    return true;
+                })
+                .Subscribe(_ =>
+                {
+                    _agent.isStopped = true;
+                    _agent.ResetPath();
+                    OnStop?.Invoke();
+                })
+                .AddTo(this);
+        }
+
+
         private void Update()
         {
             if (!_agent.pathPending)
@@ -32,9 +69,6 @@ namespace _Strategy._Main.Core
                 }
             }
         }
-        
-        
-        public IAwaiter<AsyncExtensions.Void> GetAwaiter() => new StopAwaiter(this);
         
         
         
