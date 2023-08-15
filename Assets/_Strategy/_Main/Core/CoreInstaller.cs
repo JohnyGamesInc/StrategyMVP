84fa2df752e964bba8c56c8920bfea6f623275c6
Evgenii <ivanjohnoff@gmail.com>
2023-08-15 20:17:21 +0300

    add patrol logic and game win state

--- Assets/_Strategy/_Main/Core/CoreInstaller.cs
+++ Assets/_Strategy/_Main/Core/CoreInstaller.cs
@@ -1,4 +1,6 @@
-﻿using Zenject;
+﻿using _Strategy._Main.Abstractions;
+using UnityEngine;
+using Zenject;
 
 
 namespace _Strategy._Main.Core
@@ -6,10 +8,14 @@ namespace _Strategy._Main.Core
     
     public class CoreInstaller : MonoInstaller
     {
+
+        [SerializeField] private GameStatus _gameStatus; 
+        
         
         public override void InstallBindings()
         {
             Container.BindInterfacesAndSelfTo<TimeModel>().AsSingle();
+            Container.Bind<IGameStatus>().FromInstance(_gameStatus);
         }
         
         
